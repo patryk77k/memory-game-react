cd08c8be43d1927a640388134e4de4aaac666e64
patryk77k <patryk77.k@gmail.com>
2023-08-01 17:59:01 +0200

    add schuffleCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+const cardImages = [
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png" },
+];
+
 function App() {
-  return <></>;
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  //shuffle card
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New game</button>
+    </div>
+  );
 }
 
 export default App;
